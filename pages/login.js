47a33d1408c9249ae092e9893f10747550b52b8e
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-11 15:53:29 +0530

    padding added

--- pages/login.js
+++ pages/login.js
@@ -8,7 +8,7 @@ export default function LoginPage({providers}){
                 <div>
                     
                     <button className="bg-white pl-3 pr-5 py-2 text-black gap-2 rounded-full flex items-center">
-                    <img src="/google.png" className="h-8"/>Sign in with {provider.name}</button>
+                    <img src="/google.png" className="h-8 pr-3"/>Sign in with {provider.name}</button>
                     
                 </div>
                 
